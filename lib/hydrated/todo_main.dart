71415c32abfbebfbb24844a91a3735f195eb5a09
akkiB <akash3350saha@gmail.com>
2023-04-14 12:25:38 +0530

    Hydrated bloc todo app

---
+++ lib/hydrated/todo_main.dart
@@ -0,0 +1,40 @@
+import 'package:bloc_tutorial/hydrated/presentation/todo_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
+import 'bloc/todo_bloc.dart';
+import 'data/repository/todo_repository.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getApplicationDocumentsDirectory(),
+  );
+
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => TodoRepository(),
+      child: BlocProvider(
+        create: (context) => TodoBloc(
+           context.read<TodoRepository>(),
+        ),
+          child: const MaterialApp(
+            home: TodoHome(),
+          ),
+      ),
+    );
+  }
+}
